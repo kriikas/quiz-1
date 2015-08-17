8210e652b949fd00ccb2b4cdfe45bbfc105209b0
kriikas <kri05@hotmail.com>
2015-08-17 15:52:16 +0200

    Lista de Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var models = require('../models/models.js');
 
 exports.index = function (req, res) {
@@ -11,10 +12,16 @@ exports.show = function (req, res) {
   models.Quiz.find(req.params.quizId).then(function (quiz) {
       res.render('quizes/show', { quiz: quiz});
   })
+=======
+//GET /quizes/question
+exports.question = function (req, res) {
+  res.render('quizes/question', {pregunta: 'Capital de Italia'});
+>>>>>>> parent of 3355238... Despliegue DB en Heroku
 };
 
 //GET /quizes/answer
 exports.answer = function (req, res) {
+<<<<<<< HEAD
   models.Quiz.find(req.params.quizId).then(function (quiz) {
     if(req.query.respuesta === quiz.respuesta){
       res.render('quizes/answer',
@@ -24,6 +31,13 @@ exports.answer = function (req, res) {
         { quiz: quiz, respuesta: 'Incorrecto'});
     }
   })
+=======
+  if(req.query.respuesta === "Roma"){
+    res.render('quizes/answer', {respuesta: 'Correcto'});
+  }else{
+    res.render('quizes/answer', {respuesta: 'Inorrecto'});
+  }
+>>>>>>> parent of 3355238... Despliegue DB en Heroku
 };
 exports.author = function (req, res) {
   res.render('author');
